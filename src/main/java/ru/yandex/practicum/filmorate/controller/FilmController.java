a36aa02f67f3e097a067662d0bdaa568de1f7f7d
Marina_Kuznetsova <marussia.usa@yandex.ru>
2022-05-19 10:30:15 +0300

    log moved to the service

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.film.FilmService;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController extends AbstractController<Film> {
     private final FilmService filmService;
 
@@ -16,37 +17,37 @@ public class FilmController extends AbstractController<Film> {
         this.filmService = filmService;
     }
 
-    @GetMapping("/films")
+    @GetMapping
     public List<Film> getAll() {    // возвращает список всех фильмов в ответ на GET запрос
         return filmService.getAllFilms();
     }
 
-    @PostMapping(value = "/films")
+    @PostMapping
     public Film add(@Valid @RequestBody Film film) {    // добавляет  в список новый фильм в ответ на POST запрос
         return filmService.add(film);
     }
 
-    @PutMapping(value = "/films")
+    @PutMapping
     public Film update(@Valid @RequestBody Film film) {    // обновляет данные фильма в ответ на PUT запрос
         return filmService.update(film);
     }
 
-    @GetMapping(value = "/films/{id}")
+    @GetMapping(value = "/{id}")
     public Film getById(@PathVariable(value = "id") Integer filmId) {
         return filmService.getFilmById(filmId);
     }
 
-    @PutMapping(value = "/films/{id}/like/{userId}")
+    @PutMapping(value = "/{id}/like/{userId}")
     public void addLike(@PathVariable(value = "id") Integer filmId, @PathVariable Integer userId) {
         filmService.addLike(filmId, userId);
     }
 
-    @DeleteMapping(value = "/films/{id}/like/{userId}")
+    @DeleteMapping(value = "/{id}/like/{userId}")
     public void deleteLike(@PathVariable(value = "id") Integer filmId, @PathVariable Integer userId) {
         filmService.deleteLike(filmId, userId);
     }
 
-    @GetMapping(value = "/films/popular")
+    @GetMapping(value = "/popular")
     public List<Film> getPopularFilms(@RequestParam(defaultValue = "10", required = false) Integer count) {
         return filmService.getMostPopularFilms(count);
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.filmorate.service.user.UserService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController extends AbstractController<User>{
     private final UserService userService;
 
@@ -16,42 +17,42 @@ public class UserController extends AbstractController<User>{
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAll() {    // возвращает список всех пользователей на GET запрос
         return userService.getAllUsers();
     }
 
-    @PostMapping(value = "/users")
+    @PostMapping
     public User add(@Valid @RequestBody User user) {    // добавляет нового пользователя в ответ на POST запрос
         return userService.add(user);
     }
 
-    @PutMapping(value = "/users")
+    @PutMapping
     public User update(@Valid @RequestBody User user) {    // обновляет данные пользователя в ответ на PUT запрос
         return userService.update(user);
     }
 
-    @GetMapping(value = "/users/{id}")
+    @GetMapping(value = "/{id}")
     public User getById(@PathVariable Integer id) {
         return userService.getUserById(id);
     }
 
-    @PutMapping(value = "/users/{id}/friends/{friendId}")
+    @PutMapping(value = "/{id}/friends/{friendId}")
     public void addFriend(@PathVariable(value = "id") Integer userId, @PathVariable Integer friendId) {
         userService.addFriend(userId, friendId);
     }
 
-    @DeleteMapping(value = "/users/{id}/friends/{friendId}")
+    @DeleteMapping(value = "/{id}/friends/{friendId}")
     public void deleteFriend(@PathVariable(value = "id") Integer userId, @PathVariable Integer friendId) {
         userService.deleteFriend(userId, friendId);
     }
 
-    @GetMapping(value = "/users/{id}/friends")
+    @GetMapping(value = "/{id}/friends")
     public List<User> getAllFriends(@PathVariable(value = "id") Integer userId) {
         return userService.getAllFriends(userId);
     }
 
-    @GetMapping(value = "/users/{id}/friends/common/{otherId}")
+    @GetMapping(value = "/{id}/friends/common/{otherId}")
     public List<User> getCommonFriends(@PathVariable(value = "id") Integer userId, @PathVariable Integer otherId) {
         return userService.getCommonFriends(userId, otherId);
     }
